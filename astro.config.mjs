9624eadc351bc37545cdc9cbd8423c38ac1829af
Simon Young <simon.young@lego.com>
2024-09-22 21:26:45 +0100

    Update config to use main URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://simonyoung.io',
+    site: 'http://simonyoung.io',
 });
